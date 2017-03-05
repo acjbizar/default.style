7daad9644f06353cc2b415bac363b815c412e13a
acj <gitlab@acjs.net>
2017-03-05 22:39:12 +0100

    Concat on watch.

--- _build/gruntfile.js
+++ _build/gruntfile.js
@@ -25,7 +25,7 @@ module.exports = function(grunt) {
         watch: {
             css: {
                 files: ['../sass/**/*.scss'],
-                tasks: ["sass"]
+                tasks: ["sass", "concat"]
             }
         }
     });
